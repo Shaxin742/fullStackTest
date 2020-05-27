945e4020e14d8b7dabd44edbe4c4082577902259
沙鑫 <shaxin@ysstech.com>
2020-05-27 18:08:06 +0800

    node edit

--- node-api-server/routes/users.js
+++ node-api-server/routes/users.js
@@ -7,47 +7,58 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/login',function(req,res,next){
-  const user = {
-    name:'',
-    city:'',
-    age:''
-  };
+router.post('/login',function(req,res,next){
+  const tokens = {
+    admin: {
+      token: 'admin-token'
+    },
+    editor: {
+      token: 'editor-token'
+    }
+  }
   var params = URL.parse(req.url,true).query;
 
-  if(params.id == '1'){
-      user.name = "ligh";
-      user.age = "1";
-      user.city = "北京市";
-  }else{
-      user.name = "SPTING";
-      user.age = "1";
-     user.city = "杭州市";
+  const token = tokens[params.username]
+  if (!token) {
+    return {
+      code: 400,
+      message: '用户名或密码错误'
+    }
   }
-
-  var response = {code:200,data:user};
+  var response = {code:200,data:token};
   res.send(JSON.stringify(response));
 })
 
-router.post('/info',function(req,res,next){
-  const user = {
-    name:'',
-    city:'',
-    age:''
-  };
-  var params = URL.parse(req.url,true).query;
-
-  if(params.id == '1'){
-      user.name = "ligh";
-      user.age = "1";
-      user.city = "北京市";
-  }else{
-      user.name = "SPTING";
-      user.age = "1";
-     user.city = "杭州市";
+router.get('/info',function(req,res){
+  const users = {
+    'admin-token': {
+      roles: ['admin'],
+      introduction: 'I am a super administrator',
+      avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+      name: 'Super Admin'
+    },
+    'editor-token': {
+      roles: ['editor'],
+      introduction: 'I am an editor',
+      avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+      name: 'Normal Editor'
+    }
   }
-
-  var response = {code:200,data:user};
+  var params = URL.parse(req.url,true).query;
+  const info = users[params.token]
+  if (!info) {
+    return {
+      code: 400,
+      message: '登录失败'
+    }
+  }
+  var response = {code:200,data:info};
   res.send(JSON.stringify(response));
 })
+
+router.post('/logout',function(res){
+  var response = {code:200,data:'success'};
+  res.send(JSON.stringify(response));
+})
+
 module.exports = router;
--- vue/vue.config.js
+++ vue/vue.config.js
@@ -41,8 +41,8 @@ module.exports = {
       //   }
       // }
       '/dev-api': {
-        target: `http://0.0.0.0:${port}/mock`, // 本地mock数据
-        // target: `http://127.0.0.1:5900`, // python 项目
+        // target: `http://0.0.0.0:${port}/mock`, // 本地mock数据
+        target: `http://127.0.0.1:5000`, // python 项目
         changeOrigin: true,
         pathRewrite: {
           '/app/api/dev-api': ''
