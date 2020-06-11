e2b60b8b01bb7dddb181a95d9e1e98311bf0168d
沙鑫 <shaxin@ysstech.com>
2020-06-11 12:19:35 +0800

    edit

--- vue-mock-demo/src/mock/index.js
+++ vue-mock-demo/src/mock/index.js
@@ -5,19 +5,19 @@ import dashBoard from './dashBoard.js' // 首页
 import components from './components.js' // 组件
 
 // 登录登出
-Mock.mock(/user\/login.*/, 'post', user.login)
-Mock.mock(/user\/info.*/, 'get', user.info)
-Mock.mock(/user\/logout.*/, 'post', user.logout)
+Mock.mock(/\/app\/user\/login.*/, 'post', user.login)
+Mock.mock(/\/app\/user\/info.*/, 'get', user.info)
+Mock.mock(/\/app\/user\/logout.*/, 'post', user.logout)
 
 // 首页
-Mock.mock(/dashBoard\/getCalendar.*/, 'get', dashBoard.getCalendar)
+Mock.mock(/\/app\/dashBoard\/getCalendar.*/, 'get', dashBoard.getCalendar)
 
 // 组件 components
-Mock.mock(/components\/getSongs.*/, 'get', components.getSongs)
-Mock.mock(/components\/formsubmit.*/, 'post', components.formsubmit)
-Mock.mock(/components\/getTableData.*/, 'get', components.getTableData)
-Mock.mock(/components\/deleteTableData.*/, 'post', components.deleteTableData)
-Mock.mock(/components\/addTableData.*/, 'post', components.addTableData)
-Mock.mock(/components\/updateTableData.*/, 'post', components.updateTableData)
+Mock.mock(/\/app\/components\/getSongs.*/, 'get', components.getSongs)
+Mock.mock(/\/app\/components\/formsubmit.*/, 'post', components.formsubmit)
+Mock.mock(/\/app\/components\/getTableData.*/, 'get', components.getTableData)
+Mock.mock(/\/app\/components\/deleteTableData.*/, 'post', components.deleteTableData)
+Mock.mock(/\/app\/components\/addTableData.*/, 'post', components.addTableData)
+Mock.mock(/\/app\/components\/updateTableData.*/, 'post', components.updateTableData)
 
 export default Mock
--- vue-mock-demo/src/utils/mockState.js
+++ vue-mock-demo/src/utils/mockState.js
@@ -2,11 +2,11 @@
  * @Author: ShaXin
  * @Date: 2020-05-26 14:33:14
  * @LastEditors: ShaXin
- * @LastEditTime: 2020-06-11 11:06:03
+ * @LastEditTime: 2020-06-11 11:18:33
  */
 export default {
   serverUrl(apiName) {
-    return `/app/api/${apiName}`
+    return `/api/${apiName}`
   },
   serverUrlMock(apiName) {
     return `/${apiName}`
