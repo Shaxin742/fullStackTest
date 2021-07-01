b44e3d9aa717f7815beb486f841f3ae695bb0ecf
shaxin <shaxin@didiglobal.com>
2021-07-01 16:07:26 +0800

    edit

--- egg/app/service/students.js
+++ egg/app/service/students.js
@@ -1,22 +1,32 @@
 'use strict';
 
 // app/service/students.js
-const Service = require('egg').Service;
+const Service = require('../core/BaseService');
 
 class UserService extends Service {
   async list({ name }) {
     const { ctx } = this;
     return ctx.model.Students.find({ name });
     // ctx.model.Student.find({"title":"111"}) 表示在数据库中查找title 为111的对应数据
+
+    // const { Students } = this.ctx.model;
+    // const student = new Students();
+    // student.sort = name;
+    // student.save();
   }
   // 添加用户
   async add(req) {
-    const { ctx } = this;
-    return ctx.model.Students.create(req).then(res => {
-      return { success: true, msg: res, code: 0 };
-    }).catch(err => {
-      return { success: false, err };
-    });
+    const { Students } = this.ctx.model;
+    // return Students.create(req).then(res => {
+    //   return { success: true, msg: res, code: 0 };
+    // }).catch(err => {
+    //   return { success: false, err };
+    // });
+    const student = new Students();
+    student.name = req.name;
+    student.sexy = req.sexy;
+    student.address = req.address;
+    student.save();
   }
 }
 module.exports = UserService;
